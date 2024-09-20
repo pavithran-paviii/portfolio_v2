9a52a937b7755d3f01d64f2a73c9d018d853d342
pavithran_paviii <pavithranr65@gmail.com>
2024-09-20 16:12:09 +0530

    Added focus light on navbar

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import classNames from "./navbar.module.scss";
 import { Link, useLocation } from "react-router-dom";
 import { GoArrowUpRight } from "react-icons/go";
 
 const Navbar = () => {
   const location = useLocation();
+  const focusLightRef = useRef(null);
+
+  useEffect(() => {
+    const currentNav = document.querySelector(`.${classNames.currentNav}`);
+    const focusLight = focusLightRef.current;
+    if (currentNav && focusLight) {
+      const { offsetLeft, offsetWidth } = currentNav;
+      focusLight.style.left = `${offsetLeft + offsetWidth / 2 - 17}px`;
+    }
+  }, [location]);
+
   return (
     <div className={classNames.navbar}>
       <div>Pavithran</div>
       <div className={classNames.navigations}>
+        <div className={classNames.focusLight} ref={focusLightRef}></div>
         <Link
           className={location?.pathname == "/" && classNames.currentNav}
           to="/"
