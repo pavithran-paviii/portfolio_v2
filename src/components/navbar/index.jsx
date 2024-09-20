dbc287a54d6d44bd1f985af634de8909e6481f61
pavithran_paviii <pavithranr65@gmail.com>
2024-09-21 02:56:40 +0530

    Implemented the intro landing part

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,30 +1,13 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef } from "react";
 import classNames from "./navbar.module.scss";
 import { Link, useLocation } from "react-router-dom";
 import { GoArrowUpRight } from "react-icons/go";
 import logo from "../../assets/images/logo.png";
-import { LuGithub } from "react-icons/lu";
 import { githubLink } from "../../assets/data";
 
 const Navbar = () => {
   const location = useLocation();
   const focusLightRef = useRef(null);
-
-  // is visible github
-  const [isVisible, setIsVisible] = useState(false);
-  const [position, setPosition] = useState({ top: 0, left: 0 });
-
-  const handleMouseEnter = () => {
-    setIsVisible(true);
-  };
-
-  const handleMouseMove = (e) => {
-    setPosition({ top: e.clientY - 40, left: e.clientX - 40 });
-  };
-
-  const handleMouseLeave = () => {
-    setIsVisible(false);
-  };
 
   // render for navigation menu
 
@@ -41,9 +24,6 @@ const Navbar = () => {
     <div className={classNames.navbar}>
       <div
         className={classNames.logoDiv}
-        onMouseEnter={handleMouseEnter}
-        onMouseMove={handleMouseMove}
-        onMouseLeave={handleMouseLeave}
         onClick={() => {
           window.open(githubLink, "_blank");
         }}
@@ -53,16 +33,6 @@ const Navbar = () => {
           <div>Pavithran</div>
           <div>Developer</div>
         </div>
-        {isVisible && (
-          <a
-            className={classNames.hiddenNav}
-            style={{ top: position.top, left: position.left }}
-            href={githubLink}
-            target="_blank"
-          >
-            <LuGithub />
-          </a>
-        )}
       </div>
       <div className={classNames.navigations}>
         <div className={classNames.focusLight} ref={focusLightRef}></div>
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
+import classNames from "./home.module.scss";
+import { LuGithub } from "react-icons/lu";
+import { githubLink } from "../../assets/data";
+import { FaArrowDown } from "react-icons/fa6";
 
 const Home = () => {
+  // is visible github
+  const [isVisible, setIsVisible] = useState(false);
+  const [position, setPosition] = useState({ top: 0, left: 0 });
+
+  const handleMouseEnter = () => {
+    setIsVisible(true);
+  };
+
+  const handleMouseMove = (e) => {
+    console.log(e);
+    setPosition({ top: e.clientY - 135, left: e.clientX - 135 });
+  };
+
+  const handleMouseLeave = () => {
+    setIsVisible(false);
+  };
+
   return (
-    <section>
-      <div>Home</div>
+    <section className={classNames.home}>
+      <div className={classNames.introduction}>
+        <div className={classNames.heroFade}></div>
+        <div
+          className={classNames.innerSystem}
+          onMouseEnter={handleMouseEnter}
+          onMouseMove={handleMouseMove}
+          onMouseLeave={handleMouseLeave}
+        >
+          <div className={classNames.topBar}>
+            <div className={classNames.windowDots}>
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+          </div>
+          <div className={classNames.windowContent}>
+            <div className={classNames.mainText}>
+              I build things
+              <div>for web!!</div>
+            </div>
+            <div className={classNames.currentExp}>
+              <div>Developer at Nvest Campus.</div>
+              <div>Based in Hyderabad.</div>
+            </div>
+            <FaArrowDown className={classNames.downArrow} />
+          </div>
+          {isVisible && (
+            <a
+              className={classNames.hiddenNav}
+              style={{ top: position.top, left: position.left }}
+              href={githubLink}
+              target="_blank"
+            >
+              <LuGithub />
+            </a>
+          )}
+        </div>
+      </div>
     </section>
   );
 };
